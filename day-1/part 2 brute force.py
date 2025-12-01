ce5ed75cf1960d8fd8749f77586dfb5c851ec1fc
astutulus <robinwootton@gmail.com>
2025-12-01 22:23:24 +0000

    Day 1 experiment with brute force approach

---
+++ day-1/part 2 brute force.py
@@ -0,0 +1,38 @@
+dial_position = 50
+zero_counter = 0
+
+def resolve_line(line):
+    global dial_position, zero_counter
+
+    direction = line[0]
+    distance = int(line[1:])
+
+    # Clockwise
+    if direction == "R":
+        for _ in range (distance):
+            dial_position += 1
+            if dial_position == 100:
+                dial_position = 0
+                zero_counter += 1
+
+    # Anticlockwise
+    else:
+        for _ in range (distance):
+            dial_position -= 1
+            if dial_position == 0:
+                zero_counter += 1
+            if dial_position == -1:
+                dial_position = 99
+
+def read_file_line_by_line(filepath):
+    try:
+        with open(filepath, "r", encoding="utf-8") as file:
+            for line in file:
+                resolve_line(line)
+    except FileNotFoundError:
+        print(f"Input file '{filepath}' not found")
+
+    print (f"Answer: {zero_counter}")
+
+if __name__ == "__main__":
+    read_file_line_by_line("day-1\\input.txt")
