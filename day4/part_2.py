61a0963a224ba7117784694157bb00e0a2f5e36b
astutulus <robinwootton@gmail.com>
2025-12-04 07:28:14 +0000

    Day 4 passed part 2

---
+++ day4/part_2.py
@@ -0,0 +1,72 @@
+def solve(data_path: str):
+    elves_diagram = []
+
+    # Not elegant, but doesn't cost as only called once
+    with open(data_path, "r", encoding="utf-8") as file:
+        width = len(file.readline().strip())
+
+    # Read the entire diagram into a single list
+    with open(data_path, "r", encoding="utf-8") as file:
+        for line in file:
+            for char in line.strip():
+                elves_diagram.append(char)
+
+    qty_cells = len(elves_diagram)
+
+    count = [0] * qty_cells
+
+    # NW  N  NE
+    #  W  @  E
+    # SW  S  SE    
+    west_neighbours = [-(width + 1), -1, width - 1]
+    vert_neighbours = [-width, width]
+    east_neighbours = [-(width - 1), 1, width + 1]
+
+    # For each roll of paper, increment count of all neighbouring cells
+    def count_neighbours_for_whole_diagram():
+        for cell in range(qty_cells):
+            if elves_diagram[cell] == "@":
+
+                # If cell is at edge then don't include neighbours that would wrap around
+                if cell % width == 0:  # west edge
+                    neighbours = vert_neighbours + east_neighbours
+                elif cell % width == width - 1:  # east edge
+                    neighbours = west_neighbours + vert_neighbours
+                else:
+                    neighbours = west_neighbours + vert_neighbours + east_neighbours
+
+                for n in neighbours:
+                    neighbour = cell + n
+                    # Confirm still within diagram (when handling top and bottom rows)
+                    if 0 <= neighbour < qty_cells:
+                        count[neighbour] += 1
+
+    # Remove accessible rolls of paper
+    # Return how many were removed
+    def remove_accessible_rolls() -> int:
+        removed = 0
+        for cell in range(qty_cells):
+            if elves_diagram[cell] == "@" and count[cell] < 4:
+                elves_diagram[cell] = "."
+                removed += 1
+        return removed
+
+    forklifts_working = True
+    total_rolls_removed = 0
+
+    while forklifts_working:
+        # Reset counts
+        count = [0] * qty_cells  
+        count_neighbours_for_whole_diagram()
+
+        rolls_removed = remove_accessible_rolls()
+        if rolls_removed:
+            total_rolls_removed += rolls_removed
+        else:
+            forklifts_working = False
+
+    print(f"Total rolls of paper removed: {total_rolls_removed}")
+
+#solve("day4\\debug.txt")
+#solve("day4\\example.txt")
+solve("day4\\input.txt")
