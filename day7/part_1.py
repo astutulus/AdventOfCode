95c5e5ebd4e2efa95efb0de1d3a2966a9b306bb9
astutulus <robinwootton@gmail.com>
2025-12-07 09:54:25 +0000

    Day 7 passed part 1

---
+++ day7/part_1.py
@@ -0,0 +1,29 @@
+def solve(filepath: str) -> int:
+    # Count tachylon splits
+    splits = 0
+    with open(filepath, "r", encoding="utf-8") as file:
+        beams = []
+        for line in file:
+            # First row
+            if not beams:  
+                for char in line:
+                    if char == ".":
+                        beams.append(False)
+                    elif char == "S":
+                        beams.append(True)
+            # All subsequent rows
+            else:  
+                for i, char in enumerate(line):
+                    if char == "^":
+                        if beams [i]:
+                            beams[i-1] |= True  # OR left beam
+                            beams[i] &= False  # AND NOT incoming beam
+                            beams[i+1] |= True  # OR right beam
+                            splits += 1
+    return splits
+
+if __name__ == "__main__":
+    filename = ["debug",    # index 0
+                "example",  # index 1
+                "input"]    # index 2
+    print(f"Answer: {solve(f"day7\\{filename[2]}.txt")}")  # Change index to switch data set
